a103451ea1d81101797e19d76fe6e6cb5abdf88f
Uma Uka <uma.uka@kuda.com>
2026-07-20 19:28:24 +0100

    Run all tasks via Web Console (Live Server)

--- app.js
+++
@@ -1,8 +0,0 @@
-// A function to calculate the area of a rectangle
-
-function CalculateAreaOfRectangle(length, width) {
-  const area = length * width;
-  return area;
-}
-  
-console.log("The area of the rectangle is: " + CalculateAreaOfRectangle(5, 10));
